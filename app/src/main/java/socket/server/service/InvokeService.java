7751c8b12f23db7dc85eee9fe5c8ae612c262a40
sheikhimtiaz <imtiaz@synesisit.info>
2022-02-12 23:06:16 +0600

    generic get method by name and parameter

--- app/src/main/java/socket/server/service/InvokeService.java
+++ app/src/main/java/socket/server/service/InvokeService.java
@@ -8,15 +8,25 @@ import java.lang.reflect.Method;
 
 
 public class InvokeService {
-    private static final String PREFIX = "socket.server.manager.";
+    private static final String MANAGER_PATH = "socket.server.manager.";
 
     public int invokeMethodWithManagerName(RequestObject requestObject) throws ClassNotFoundException, NoSuchMethodException, InvocationTargetException, InstantiationException, IllegalAccessException {
-        Class<?> c = Class.forName(PREFIX + requestObject.managerName);
+        Class<?> c = Class.forName(MANAGER_PATH + requestObject.managerName);
         Constructor<?> cons = c.getConstructor();
         Object object = cons.newInstance();
 
-        Method method = object.getClass().getMethod(requestObject.method, int.class);
+        Method method = getMethodWithNameAndParameter(requestObject, object);
 
-        return (int)method.invoke(object, Integer.parseInt((String)requestObject.args.get("n")));
+        return (method!=null) ? (int)method.invoke(object, Integer.parseInt((String)requestObject.args.get("n"))) : -1;
     }
+
+    private Method getMethodWithNameAndParameter(RequestObject requestObject, Object object){
+        for (Method method : object.getClass().getMethods()) {
+            if (method.getName().equals(requestObject.method) && (method.getParameters().length == requestObject.args.size())) {
+                return method;
+            }
+        }
+        return null;
+    }
+
 }
